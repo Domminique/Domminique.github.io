b0a389e20264aec1847370481afd8a9d024350fa
Domminique <imbugad@gmail.com>
2015-11-19 11:27:26 +0300

    add photo on navbar

--- js/jquerryuiwork.js
+++ js/jquerryuiwork.js
@@ -3,5 +3,30 @@ $(document).ready(function()
 $("#accordion").accordion();
 $( "#tabs" ).tabs();
 
+var listTemplate='
 
+
+      <ul>
+        <li>
+        	<h1><a href="#repo_link">Repository title</a></h1>
+        	<p>Description</p>
+        	<span>Last Update:<time>12:00pm</time></span>
+        </li>
+
+        </ul>
+
+
+        ';
+
+       /* var listTemplate='';
+        listTemplate +='<ul>';
+        listTemplate += '<li>';
+        listTemplate += '<h1><a href="#repo_link">Repository title</a></h1>';
+        listTemplate += '<p>Description</p>';
+        listTemplate += '<span>Last Update:<time>12:00pm</time></span>';
+        listTemplate += '</li>';
+        listTemplate += ' </ul>';
+        $('#open-source-container').html(listTemplate);
+
+*/
 });
